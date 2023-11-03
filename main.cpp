bd34eb3f40596477636844a3a3d0d70b0db3f6cc
Ziyad Hassan <ziyadhassan500@gmail.com>
2023-11-03 20:19:34 +0200

    adding getmin function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 
-int getSum(std::vector<int> vec) {
+int getSum(std::vector<int> vec)
+{
     int sum = 0;
-    for (int num : vec) {
+    for (int num : vec)
+    {
         sum += num;
     }
     return sum;
@@ -11,11 +13,21 @@ int getSum(std::vector<int> vec) {
 
 float getAvg(std::vector<int> vec)
 {
-    float sum  = getSum(vec);
-    return sum/vec.size();
+    float sum = getSum(vec);
+    return sum / vec.size();
 }
-
-int main() {
+int getmin(std::vector<int> vec)
+{
+    int mn = 1e5;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (mn > vec[i])
+            mn = vec[i];
+    }
+    return mn;
+}
+int main()
+{
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::cout << "Sum: " << getSum(numbers) << std::endl;
     std::cout << "Avg: " << getAvg(numbers) << std::endl;
